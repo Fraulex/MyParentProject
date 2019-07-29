77b1d274631ea44bbf0aacfece9c397f864a82b5
Marando <francesca.marando@accenture.com>
2019-07-29 17:10:08 +0200

    First Commit

---
+++ my-model-project/src/main/java/it/accenture/progetto/App.java
@@ -0,0 +1,13 @@
+package it.accenture.progetto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
